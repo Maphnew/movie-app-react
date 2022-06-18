f2e0b7b5f47f0f7885d3286d5cd349d62e98508d
maphnew <zcm3118@gmail.com>
2022-06-18 17:29:43 +0900

    styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav id="globalnav" className="navigation">
+        <div class="gn-content">
+          <ul class="gn-list">
+            <li class="gn-item gn-home"></li>
+            <li class="gn-item gn-item-menu gn-movie"></li>
+            <li class="gn-item gn-item-menu gn-musin"></li>
+          </ul>
+        </div>
+      </nav>
+      <header className="header"></header>
+      <main className="main"></main>
+      <footer className="footer"></footer>
     </div>
   );
 }
