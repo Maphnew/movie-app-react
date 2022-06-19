d217294789f5982cdc988368b5a4544b9f4edec0
maphnew <zcm3118@gmail.com>
2022-06-19 11:32:52 +0900

    header

--- src/App.js
+++ src/App.js
@@ -3,16 +3,9 @@ import "./styles/App.css";
 function App() {
   return (
     <div className="App">
-      <nav id="globalnav" className="navigation">
-        <div class="gn-content">
-          <ul class="gn-list">
-            <li class="gn-item gn-home"></li>
-            <li class="gn-item gn-item-menu gn-movie"></li>
-            <li class="gn-item gn-item-menu gn-musin"></li>
-          </ul>
-        </div>
-      </nav>
-      <header className="header"></header>
+      <header className="header">
+        <p className="header__title">MOVIE APP</p>
+      </header>
       <main className="main"></main>
       <footer className="footer"></footer>
     </div>
